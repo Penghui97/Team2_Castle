786a1e5e5adc863489a1206ad7c98cb87d96297d
J.Cheng <91435714+TheShadowsD@users.noreply.github.com>
2022-04-17 15:18:15 +0100

    update castle details activity

--- app/src/main/java/com/example/mywork2/CastleFragment/PanelOverCastleFragment.java
+++ app/src/main/java/com/example/mywork2/CastleFragment/PanelOverCastleFragment.java
@@ -2,17 +2,22 @@ package com.example.mywork2.CastleFragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.mywork2.R;
 
 public class PanelOverCastleFragment extends Fragment {
+    private String castleName;
 
-    public PanelOverCastleFragment() {
+    public PanelOverCastleFragment(String castleName) {
+        this.castleName=castleName;
         // Required empty public constructor
     }
 
@@ -21,5 +26,29 @@ public class PanelOverCastleFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_panel_over_castle, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ImageView imageView = view.findViewById(R.id.castle_picture);
+        switch (castleName){
+            case "alnwic":
+                imageView.setImageResource(R.drawable.alnwic1);
+                break;
+            case "auckland":
+                imageView.setImageResource(R.drawable.auckland1);
+                break;
+            case "barnard":
+                imageView.setImageResource(R.drawable.barnard);
+                break;
+            case "bamburgh":
+                imageView.setImageResource(R.drawable.bamburgh);
+                break;
+            default:
+                break;
+        }
+
+
     }
 }
