f9c0579321cfb962d08fbe1bcb0af7a7b886d045
Penghui <1015899521@qq.com>
2022-04-17 15:45:34 +0100

    login fragment

--- app/src/main/java/com/example/mywork2/LogInFragment/UserLogInFragment.java
+++ app/src/main/java/com/example/mywork2/LogInFragment/UserLogInFragment.java
@@ -2,17 +2,21 @@ package com.example.mywork2.LogInFragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.mywork2.R;
 
 public class UserLogInFragment extends Fragment {
-
-
+    private EditText username, password;
+    private TextView username_warn, password_warn;
 
     public UserLogInFragment() {
         // Required empty public constructor
@@ -24,5 +28,13 @@ public class UserLogInFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user__log__in_, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        username = view.findViewById(R.id.username_login);
+
     }
 }
