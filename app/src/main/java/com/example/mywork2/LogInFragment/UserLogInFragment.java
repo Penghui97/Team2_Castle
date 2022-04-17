9585760fce128c8cf51bcde817ed456521ca62e0
J.Cheng <91435714+TheShadowsD@users.noreply.github.com>
2022-04-17 16:02:28 +0100

    add a forget password fragment

--- app/src/main/java/com/example/mywork2/LogInFragment/UserLogInFragment.java
+++ app/src/main/java/com/example/mywork2/LogInFragment/UserLogInFragment.java
@@ -2,15 +2,21 @@ package com.example.mywork2.LogInFragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.mywork2.R;
 
-public class UserLogInFragment extends Fragment {
+public class UserLogInFragment extends Fragment implements View.OnClickListener{
+    NavController navController;
 
 
 
@@ -24,5 +30,25 @@ public class UserLogInFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user__log__in_, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController= Navigation.findNavController(view);
+
+        TextView forgetPassword = view.findViewById(R.id.forget_password);
+        forgetPassword.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.forget_password:
+                navController.navigate(R.id.action_userLogInFragment_to_forgetPasswordFragment);
+                break;
+        }
     }
 }
