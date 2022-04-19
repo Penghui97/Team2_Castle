8a7b0b1a7e8c6ef041e93cc638c7835eb05168e4
J.Cheng <91435714+TheShadowsD@users.noreply.github.com>
2022-04-19 13:43:03 +0100

    update the account edit page

--- app/src/main/java/com/example/mywork2/Avatar.java
+++ app/src/main/java/com/example/mywork2/Avatar.java
@@ -11,7 +11,6 @@ import androidx.core.content.FileProvider;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
-import android.app.Activity;
 import android.content.ContentUris;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -25,19 +24,15 @@ import android.os.Bundle;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mywork2.Util.ImageUtil;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -64,7 +59,7 @@ public class Avatar extends AppCompatActivity {
         //set bottom view
         bottomSheetDialog = new BottomSheetDialog(Avatar.this,R.style.BottomSheetDialogTheme);
         View bottomView = LayoutInflater.from(getApplicationContext()).inflate(
-                R.layout.avatar_bottom_sheet,
+                R.layout.bottom_sheet_avatar,
                 findViewById(R.id.avatar_bottom_sheet)
         );
 
--- app/src/main/java/com/example/mywork2/MyAccount/AppSettingsActivity.java
+++ app/src/main/java/com/example/mywork2/MyAccount/AppSettingsActivity.java
@@ -3,11 +3,15 @@ package com.example.mywork2.MyAccount;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.mywork2.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
-public class AppSettingsActivity extends AppCompatActivity {
+public class AppSettingsActivity extends AppCompatActivity implements View.OnClickListener{
+    private BottomSheetDialog bottomSheetDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,13 @@ public class AppSettingsActivity extends AppCompatActivity {
         findViewById(R.id.back_button).setOnClickListener(view -> {
             this.finish()
             ;});
+        
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
