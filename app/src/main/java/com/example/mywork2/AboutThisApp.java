e520cc53c59f74bee0c943c881745a8e7e32540d
J.Cheng <91435714+TheShadowsD@users.noreply.github.com>
2022-04-21 18:33:16 +0100

    update about this app

--- app/src/main/java/com/example/mywork2/AboutThisApp.java
+++ app/src/main/java/com/example/mywork2/AboutThisApp.java
@@ -2,7 +2,9 @@ package com.example.mywork2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutThisApp extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class AboutThisApp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_this_app);
+
+        TextView title = findViewById(R.id.header_title);
+        title.setText(R.string.About_this_app);
+        title.setTextColor(Color.BLACK);
+        findViewById(R.id.back_button).setOnClickListener(view -> {
+            this.finish()
+            ;});
     }
 }
