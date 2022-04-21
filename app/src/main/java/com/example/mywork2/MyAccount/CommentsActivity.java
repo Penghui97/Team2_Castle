eac6dcc1d31e230de7c8d5b74ce42d3796d571db
J.Cheng <91435714+TheShadowsD@users.noreply.github.com>
2022-04-21 21:02:43 +0100

    update comments xml

---
+++ app/src/main/java/com/example/mywork2/MyAccount/CommentsActivity.java
@@ -0,0 +1,24 @@
+package com.example.mywork2.MyAccount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.mywork2.R;
+
+public class CommentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comments);
+
+        TextView title = findViewById(R.id.header_title);
+        title.setText(R.string.comment);
+        findViewById(R.id.back_button).setOnClickListener(view -> {
+            this.finish()
+            ;});
+
+    }
+}
