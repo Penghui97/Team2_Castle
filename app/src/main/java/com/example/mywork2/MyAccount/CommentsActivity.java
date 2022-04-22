80794aa100306cda7a99b2c7ee3f2806bb7a1226
J.Cheng <91435714+TheShadowsD@users.noreply.github.com>
2022-04-22 13:22:09 +0100

    comments

---
+++ app/src/main/java/com/example/mywork2/MyAccount/CommentsActivity.java
@@ -0,0 +1,13 @@
+package com.example.mywork2.MyAccount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
