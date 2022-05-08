102b2f7e0c6a365c122334358f7502bb2fae72ed
Penghui <1015899521@qq.com>
2022-05-08 17:27:57 +0100

    fix forgot password bug and login bug

--- app/src/main/java/com/example/mywork2/LogInFragment/UserLogInFragment.java
+++ app/src/main/java/com/example/mywork2/LogInFragment/UserLogInFragment.java
@@ -1,24 +1,34 @@
 package com.example.mywork2.LogInFragment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.os.Handler;
+import android.os.Message;
+import android.text.InputType;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mywork2.LogInActivity;
 import com.example.mywork2.MainActivity;
 import com.example.mywork2.R;
+import com.example.mywork2.Util.ImageUtil;
 import com.example.mywork2.Util.PasswordUtil;
 import com.example.mywork2.dao.UserDao;
 import com.example.mywork2.domain.User;
@@ -28,7 +38,7 @@ import java.io.UnsupportedEncodingException;
 public class UserLogInFragment extends Fragment {
     private EditText username, password;
     private TextView username_warn;
-    private TextView password_warn;
+    private TextView password_warn, forgot, forgot_warn;
     Button login,back;
     NavController navController;
     String rememberName, rememberPassword;
@@ -37,6 +47,39 @@ public class UserLogInFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what) {
+                case 0x11://no username found
+                    username_warn.setText(R.string.please_enter_username);
+                    break;
+                case 0x22://username is too long
+                    username_warn.setText(R.string.username_too_long);
+                    break;
+                case 0x33://no password found
+                    password_warn.setText(R.string.enter_password);
+                    break;
+                case 0x44://password too long
+                    password_warn.setText(R.string.password_long);
+                    break;
+                case 0x55://wrong email address
+                    username_warn.setText(R.string.email_end);
+                    break;
+                case 0x66://no email found in db
+                    username_warn.setText(R.string.emailhasnotbeenregistered);
+                    break;
+                case 0x77://password is wrong
+                    password_warn.setText(R.string.passwordwrong);
+                    break;
+                case 0x88://no username found in db
+                    username_warn.setText(R.string.usernamenotregister);
+                    break;
+            }
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,6 +89,7 @@ public class UserLogInFragment extends Fragment {
 
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -54,7 +98,12 @@ public class UserLogInFragment extends Fragment {
         username_warn = view.findViewById(R.id.username_login_warn);
         password = view.findViewById(R.id.password_login);
         password_warn = view.findViewById(R.id.password_login_warn);
+        forgot_warn = view.findViewById(R.id.pass_forgot_warn);
 
+
+        navController = Navigation.findNavController(view);
+        forgot = view.findViewById(R.id.forget_password);
+        forgot.setOnClickListener(view1 -> navController.navigate(R.id.action_userLogInFragment_to_forgetPasswordFragment));
         //get data from login activity
         Bundle bundle = getArguments();
         if(bundle!=null){
@@ -62,6 +111,18 @@ public class UserLogInFragment extends Fragment {
             rememberPassword = bundle.getString("password");
             username.setText(rememberName);
             password.setText(rememberPassword);
+            forgot_warn.setTextColor(Color.BLUE);
+            forgot_warn.setText(R.string.findback);
+
+            //show password
+            forgot.setOnTouchListener((view1, motionEvent) -> {
+                if (motionEvent.getAction()== MotionEvent.ACTION_DOWN)
+                    password.setInputType(InputType.TYPE_CLASS_TEXT);
+                if (motionEvent.getAction()== MotionEvent.ACTION_UP)
+                    password.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                return true;
+            });
+
         }
         //buttons
         back = view.findViewById(R.id.btn_back_);
@@ -78,10 +139,6 @@ public class UserLogInFragment extends Fragment {
         }).start());
 
 
-        navController = Navigation.findNavController(view);
-        TextView forgot = view.findViewById(R.id.forget_password);
-        forgot.setOnClickListener(view1 -> navController.navigate(R.id.action_userLogInFragment_to_forgetPasswordFragment));
-
 
 
     }
@@ -96,20 +153,30 @@ public class UserLogInFragment extends Fragment {
             //referenced from https://blog.csdn.net/qq_60750453/article/details/123709670
             String reg = getString(R.string.reg);
             if(username.getText().toString().length() == 0 ) {//no username found
-                username_warn.setText(R.string.please_enter_username);
+                Message message = handler.obtainMessage();
+                message.what = 0x11;
+                handler.sendMessage(message);
                 return;
             }else if (username.getText().toString().length()>40){//username's length should be less than 40
-                username_warn.setText(R.string.username_too_long);
+                Message message = handler.obtainMessage();
+                message.what = 0x22;
+                handler.sendMessage(message);
                 return;
             }else if(password.getText().toString().length()==0) {//no password found
-                password_warn.setText(R.string.enter_password);
+                Message message = handler.obtainMessage();
+                message.what = 0x33;
+                handler.sendMessage(message);
                 return;
             }else if(password.getText().toString().length()>16){//password's length should be less than 16
-                password_warn.setText(R.string.password_long);
+                Message message = handler.obtainMessage();
+                message.what = 0x44;
+                handler.sendMessage(message);
                 return;
             }else if(username.getText().toString().contains("@")&&(!username.getText().toString().matches(reg)
             ||!username.getText().toString().endsWith(".ac.uk"))){//wrong email address
-                username_warn.setText(R.string.email_end);
+                Message message = handler.obtainMessage();
+                message.what = 0x55;
+                handler.sendMessage(message);
                 return;
             }else {
                 //check the database and verify
@@ -121,11 +188,15 @@ public class UserLogInFragment extends Fragment {
                     try {
                         user = userDao.getUserByEmail(name);
                         if(user == null) {//no user found
-                            username_warn.setText(R.string.emailhasnotbeenregistered);
+                            Message message = handler.obtainMessage();
+                            message.what = 0x66;
+                            handler.sendMessage(message);
                             return;
                         }else {
                             if (!plain_password.equals(PasswordUtil.hex2Str(user.getPassword()))){//password is wrong
-                                password_warn.setText(R.string.passwordwrong);
+                                Message message = handler.obtainMessage();
+                                message.what = 0x77;
+                                handler.sendMessage(message);
 
                             }else {
                                 information = true;
@@ -139,11 +210,15 @@ public class UserLogInFragment extends Fragment {
                     try {
                         user = userDao.getUserByUsername(name);
                         if(user == null) {//no user found
-                            username_warn.setText(R.string.usernamenotregister);
+                            Message message = handler.obtainMessage();
+                            message.what = 0x88;
+                            handler.sendMessage(message);
                             return;
                         }else {
                             if (!plain_password.equals(PasswordUtil.hex2Str(user.getPassword()))){//password is wrong
-                                password_warn.setText(R.string.passwordwrong);
+                                Message message = handler.obtainMessage();
+                                message.what = 0x77;
+                                handler.sendMessage(message);
                             }else {
                                 information = true;
                             }
